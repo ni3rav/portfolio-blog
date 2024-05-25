691a4501abbde081a894b4fcfd307f022a67d46b
nirav maheta <mahetan35@gmail.com>
2024-05-25 12:10:50 +0530

    configuring site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://ni3rav.me',
     integrations: [
         mdx(),
         sitemap(),
